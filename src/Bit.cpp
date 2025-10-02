2fbad09be2d148b7c0524bcabbf716c883bb093f
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-10-02 09:08:48 +0200

    OK for 2025 Version

--- src/Bit.cpp
+++ src/Bit.cpp
@@ -3,24 +3,31 @@
  * 	@brief 	Bit class definition file
  * 	@author	Alexis ROLLAND
  * 	@date	2023-01
- * 
- */	
+ *
+ */
 #include "Bit.hpp"
 //----------------------------------------------------------------------
-std::string_view	Bit::toString() const noexcept{
-	switch(this->getValue()){ 
-		case bit_t::SET: return "SET";break;
-		case bit_t::CLEAR: return "CLEAR";break;
-		case bit_t::HIZ: return "HIZ";break;
-		case bit_t::X: return "X";break;
-		default: return "";
-		}
-	
-}	
+std::string_view Bit::toString() const noexcept {
+    switch (this->getValue()) {
+    case bit_t::SET:
+        return "SET";
+        break;
+    case bit_t::CLEAR:
+        return "CLEAR";
+        break;
+    case bit_t::HIZ:
+        return "HIZ";
+        break;
+    case bit_t::X:
+        return "X";
+        break;
+    default:
+        return "";
+    }
+}
 //----------------------------------------------------------------------
-std::ostream& operator<<(std::ostream& os, const Bit& bit) noexcept {  	
-	os << bit.toString();	/**< 	<< operator overloaded - redirecting to toString method. */
-	return os;
-	}	 
+std::ostream &operator<<(std::ostream &os, const Bit &bit) noexcept {
+    os << bit.toString(); /**< 	<< operator overloaded - redirecting to toString method. */
+    return os;
+}
 //----------------------------------------------------------------------
-
